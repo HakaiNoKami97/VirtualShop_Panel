09e773f63c00f73d9b257b2e149b3f519c2c52fa
HakaiNoKami97 <nokamihakai@gmail.com>
2023-04-12 06:31:16 -0500

    Upload Project

---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import LoginApp from '../views/LoginApp.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {path: '/',name: 'home',component: LoginApp},
+  {path: '/about',name: 'about'}
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
