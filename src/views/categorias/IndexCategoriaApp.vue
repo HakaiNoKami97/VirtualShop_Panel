d82813b7ab73ed379df4c073c8014131ff7179ba
HakaiNoKami97 <nokamihakai@gmail.com>
2023-05-24 13:59:37 -0500

    Listar Categorias

--- src/views/categorias/IndexCategoriaApp.vue
+++ src/views/categorias/IndexCategoriaApp.vue
@@ -49,7 +49,7 @@
                           <div class="card">
                               <div class="card-body">
                                   <ul class="list-group list-group-flush list my-n3">
-                                      <li class="list-group-item">
+                                      <li class="list-group-item" v-for="item in categorias">
                                           <div class="row align-items-center">
                                               <div class="col-auto">
   
@@ -63,12 +63,16 @@
   
                                                   <!-- Title -->
                                                   <h4 class="mb-1 name">
-                                                      <a href="profile-posts.html">Miyah Myles</a>
+                                                      <a href="profile-posts.html">{{item.titulo}}</a>
                                                   </h4>
   
                                                   <!-- Time -->
-                                                  <p class="small mb-0">
-                                                      <span class="text-success">●</span> Online
+                                                  <p class="small mb-0" v-if="item.estado">
+                                                      <span class="text-success">●</span> Publicado
+                                                  </p>
+  
+                                                  <p class="small mb-0" v-if="!item.estado">
+                                                      <span class="text-danger">●</span> Oculto
                                                   </p>
   
                                               </div>
@@ -111,127 +115,7 @@
                                               </div>
                                           </div>
                                       </li>
-                                      <li class="list-group-item">
-                                          <div class="row align-items-center">
-                                              <div class="col-auto">
-  
-                                                  <!-- Avatar -->
-                                                  <a href="profile-posts.html" class="avatar">
-                                                              <img src="https://dashkit.goodthemes.co/assets/img/avatars/profiles/avatar-5.jpg" alt="..." class="avatar-img rounded-circle">
-                                                          </a>
-  
-                                              </div>
-                                              <div class="col ms-n2">
-  
-                                                  <!-- Title -->
-                                                  <h4 class="mb-1 name">
-                                                      <a href="profile-posts.html">Ryu Duke</a>
-                                                  </h4>
-  
-                                                  <!-- Time -->
-                                                  <p class="small mb-0">
-                                                      <span class="text-success">●</span> Online
-                                                  </p>
-  
-                                              </div>
-                                              <div class="col-auto">
-  
-                                                  <a href="#!" class="btn btn-sm btn-danger text-white" style="margin-right: 1rem;">
-                                                      Quitar
-                                                  </a>
-                                                  <a href="#!" class="btn btn-sm btn-dark text-white">
-                                                      Subcategoria
-                                                  </a>
-                                              </div>
-                                          </div>
-                                          <!-- / .row -->
-                                          <div class="row mb-3">
-                                              <div class="col-12">
-                                                  <ul class="list-group mt-4">
-                                                      
-                                                      <li class="list-group-item d-flex justify-content-between align-items-center" style="font-size: .8rem;padding: 0.5rem 1.5rem;">
-                                                          Morbi leo risus 
-                                                          <a href="#!" class="btn btn-sm btn-danger text-white">
-                                                              Quitar
-                                                          </a>
-                                                      </li>
-                                                      <li class="list-group-item d-flex justify-content-between align-items-center" style="font-size: .8rem;padding: 0.5rem 1.5rem;">
-                                                          Morbi leo risus 
-                                                          <a href="#!" class="btn btn-sm btn-danger text-white">
-                                                              Quitar
-                                                          </a>
-                                                      </li>
-                                                      <li class="list-group-item d-flex justify-content-between align-items-center" style="font-size: .8rem;padding: 0.5rem 1.5rem;">
-                                                          Morbi leo risus 
-                                                          <a href="#!" class="btn btn-sm btn-danger text-white">
-                                                              Quitar
-                                                          </a>
-                                                      </li>
-                                                  </ul>
-                                              </div>
-                                          </div>
-                                      </li>
-                                      <li class="list-group-item">
-                                          <div class="row align-items-center">
-                                              <div class="col-auto">
-  
-                                                  <!-- Avatar -->
-                                                  <a href="profile-posts.html" class="avatar">
-                                                              <img src="https://dashkit.goodthemes.co/assets/img/avatars/profiles/avatar-5.jpg" alt="..." class="avatar-img rounded-circle">
-                                                          </a>
-  
-                                              </div>
-                                              <div class="col ms-n2">
-  
-                                                  <!-- Title -->
-                                                  <h4 class="mb-1 name">
-                                                      <a href="profile-posts.html">Glen Rouse</a>
-                                                  </h4>
-  
-                                                  <!-- Time -->
-                                                  <p class="small mb-0">
-                                                      <span class="text-warning">●</span> Busy
-                                                  </p>
-  
-                                              </div>
-                                              <div class="col-auto">
-  
-                                                  <a href="#!" class="btn btn-sm btn-danger text-white" style="margin-right: 1rem;">
-                                                      Quitar
-                                                  </a>
-                                                  <a href="#!" class="btn btn-sm btn-dark text-white">
-                                                      Subcategoria
-                                                  </a>
-  
-                                              </div>
-                                          </div>
-                                          <!-- / .row -->
-                                          <div class="row mb-3">
-                                              <div class="col-12">
-                                                  <ul class="list-group mt-4">
-                                                      
-                                                      <li class="list-group-item d-flex justify-content-between align-items-center" style="font-size: .8rem;padding: 0.5rem 1.5rem;">
-                                                          Morbi leo risus 
-                                                          <a href="#!" class="btn btn-sm btn-danger text-white">
-                                                              Quitar
-                                                          </a>
-                                                      </li>
-                                                      <li class="list-group-item d-flex justify-content-between align-items-center" style="font-size: .8rem;padding: 0.5rem 1.5rem;">
-                                                          Morbi leo risus 
-                                                          <a href="#!" class="btn btn-sm btn-danger text-white">
-                                                              Quitar
-                                                          </a>
-                                                      </li>
-                                                      <li class="list-group-item d-flex justify-content-between align-items-center" style="font-size: .8rem;padding: 0.5rem 1.5rem;">
-                                                          Morbi leo risus 
-                                                          <a href="#!" class="btn btn-sm btn-danger text-white">
-                                                              Quitar
-                                                          </a>
-                                                      </li>
-                                                  </ul>
-                                              </div>
-                                          </div>
-                                      </li>
+                                    
                                   </ul>
                               </div>
                           </div>
@@ -260,6 +144,7 @@
       return {
           section_form: false,
           nueva_categoria: '',
+          categorias: []
       }
     },
     methods: {
@@ -289,7 +174,20 @@
               }
         
           });
+      },
+      init_data(){
+          axios.get(this.$url+'/listar_categorias_admin',{
+              headers:{
+                  'Content-Type': 'application/json',
+                  'Authorization': this.$store.state.token,
+              }
+          }).then((result)=>{
+              this.categorias = result.data;
+          });
       }
+    },
+    beforeMount() {
+      this.init_data();
     },
     components: {
       Sidebar,
